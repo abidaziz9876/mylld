8d2779129a2fdedfa5ead02da0a5dd1ae9594d09
Aziz <abidaziz9876@gmail.com>
2025-05-31 07:49:18 +0530

    added movie ticket booking system

---
+++ movieticketbookingsystem/UPIPayment.java
@@ -0,0 +1,15 @@
+public class UPIPayment implements PaymentStrategy{
+    private String upiId;
+
+    public UPIPayment(String upiId) {
+        this.upiId = upiId;
+    }
+
+    
+
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Payment successfull of amount "+amount+" with upi id "+upiId);
+        return true;
+    }
+}
